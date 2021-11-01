f9b50dd49b283839857f544004c9110a9992b8d3
rambkk <37767945+rambkk@users.noreply.github.com>
2021-11-01 17:30:55 +0700

    added 3 recursive functions working together

--- Javascript-sumArray.js
+++ Javascript-sumArray.js
@@ -236,6 +236,24 @@ function sumArrays(...a) {
                                         [sumArrays(...sumArrays(...a,true)),...sumArrays(...a)]:[]
 }
 
+/* 3 recursive functions working together 
+ * addItems just add value of array values together 
+ * array1Col remove 1st item from arrays and return the array of the removed items
+ * sumArrays just check if the item is an array and process accordngly
+ * NOTE: works on multi dimension arrays
+ * NOTE: process many arrays 
+ * NOTE: arrays can have different sizes but must be same structure
+ * NOTE: if the corresponding value does not exist, use 0 when adding
+ * NOTE: will not work properly if array structures do not match
+ */
+function addItems(...a) { return a[0].length ?a[0].shift() + addItems(...a):0 }
+function array1Col(...a) { return a.length ? [  ...a[0].length?[a[0].shift()]:[] , ...arrayItem(...a.slice(1)) ]:[]; }
+function sumArrays(...a) {
+        return !a.flat(Infinity).length?[]:
+                [ (!Array.isArray(a.flat()[0]) ?  addItems(array1Col(...a)) : sumArrays(...array1Col(...a)))
+                        , ...sumArrays(...a) ]
+}
+
 
 /* Using simple iteration for 2-dimension
  * Adding linear arrays OR 2-dimension array with same number of items (ignoring extra items in b) 
